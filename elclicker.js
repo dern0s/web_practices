66a2c97134efa520b6f7f776d1049fc571b9562f
Denis Alves <denis.alves.hu@gmail.com>
2021-05-10 21:08:26 -0300

    el clicker now works with toggle, noice

--- elclicker.js
+++ elclicker.js
@@ -1,55 +1,34 @@
-//refactor two: separate script and style into different archives
-
-//refactor one: group common code into reusable function
+//basic function to reuse code
 function changeDisplay(targetId, intendedStyle) {
     document.getElementById(targetId).style.display = intendedStyle;
 }
 
-/*hol up, this doesnt make too much sense but it will eventually
-it WILL toggle eventually
-let itsOff = document.getElementById("not").style.display == "none";
-^this doesn't work because it doesn't catch the properties from css file
-console.log(itsOff);
-this comes as false even with the property being this on the css file, apparently it's not acessible
-from here
-console.log(document.getElementById("not").style.display);
-console.log(document.getElementById("not").style);
-both of these come as an empty string but apparently it comes right after changing the style
-with DOM*/
+//harold is being used as a state for the image, when the page loads it's defaulted to false
+//this is important to use into the inner if statement
+let harold = false;
 
-
+//here we add a click event into the header to change the page
 document.getElementsByClassName("maire")[0].addEventListener("click", () => {
 
-    //refactor three: enable togglingg between harold and !harold
+    //if harold image isn't displayed it turns it visible
+    if (!harold) {
+
+        changeDisplay("not", "inline");
+
+        changeDisplay("harold", "block");
+        //this block makes harold visible, so we'll say it's true now
+        harold = true;
+
+        changeDisplay("un", "inline");
     
-    //this doesn't work because what it's on 8-18
-    //if (itsOff){
+    } else {
 
-    changeDisplay("not", "inline");
-    //old way:
-    //document.getElementById("not").style.display = "inline";
-
-    changeDisplay("harold", "block");
-    //old way:
-    //document.getElementById("harold").style.display = "block";
-
-    changeDisplay("un", "inline");
-    //master debug method:
-    //console.log(document.getElementById("not").style.display);
-    
-    //same as 26
-    //} else {
-    //this should work, since when changing stuff with DOM leaves a trace
-    //now we have a different problem, it's changing both at the same time
-    //I'll think of something later
-    /*if (document.getElementById("not").style.display == "inline") {
-        
         changeDisplay("not", "none");
 
         changeDisplay("harold", "none");
+        //harold is not displayed, so we'll say it's false
+        harold = false;
 
-        changeDisplay("un", "none"); 
-    
-    }*/
-
+        changeDisplay("un", "none");
+    }
 });
